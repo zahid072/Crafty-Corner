df1b17030fee92068b90df2c8a455a16d4575bc0
zahid072 <jahidhasan19368@gmail.com>
2024-04-26 11:20:51 +0600

    assignment 10 client side setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import './App.css'
+
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/router";
+import AuthProvider from "./provider/AuthProvider";
+
+function App() {
+  return (
+    <>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/layout/Root.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Root = () => {
+  return (
+    <div>
+        <div>
+
+        </div>
+      <div>
+        <Outlet/>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Root
